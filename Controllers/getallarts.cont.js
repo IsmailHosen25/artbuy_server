3ea81f20dc2181bd932573f2c8f10a814e05e27e
Md. Ismail Hosen <ismailhosen1006@gmail.com>
2024-12-19 15:34:31 +0600

    home art show server added

---
+++ Controllers/getallarts.cont.js
@@ -0,0 +1,14 @@
+const arts=require("../models/arts.mod")
+const getallarts=async(req,res)=>{
+  try{
+      const allarts=await arts.find().populate("userid","username -_id")
+      res.json({
+        "request":"Accepted",
+        "data":allarts
+      })
+  }
+  catch(e){
+    res.json({"error":e.message})
+  }
+}
+module.exports=getallarts
